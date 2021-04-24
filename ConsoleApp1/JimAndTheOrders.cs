23d4c679dac3cd8b73034d082f3fe0ae068e43f1
javi.dombro <javi.dombro@outlook.com>
2021-04-24 16:20:11 -0300

    Jim and the Orders

---
+++ ConsoleApp1/JimAndTheOrders.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRankSolutions
+{
+    public static class JimAndTheOrders
+    {
+
+        public static List<int> jimOrders(List<List<int>> orders)
+        {
+            var deliveredTimes = new List<KeyValuePair<int, int>>();
+
+            for (int i = 0; i < orders.Count; i++)
+            {
+                var serveTime = orders[i][1] + orders[i][0];
+                deliveredTimes.Add(new KeyValuePair<int, int>(serveTime, i + 1));
+            }
+            return deliveredTimes.OrderBy(d => d.Key).Select(o => o.Value).ToList();
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,13 +1,18 @@
-﻿namespace HackerRankSolutions
+﻿using System.Collections.Generic;
+
+namespace HackerRankSolutions
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var order = JimAndTheOrders.jimOrders(new List<List<int>>
+            {
+                new List<int>{1,13},new List<int>{2,3}, new List<int>{2,2}, new List<int>{3,3}
+            });
 
-
-            var smallest = ReverseShuffleMerge.reverseShuffleMerge("aeiouuoiea");
-            System.Console.WriteLine(smallest);
+            //var smallest = ReverseShuffleMerge.reverseShuffleMerge("aeiouuoiea");
+            //System.Console.WriteLine(smallest);
         }
     }
 }
