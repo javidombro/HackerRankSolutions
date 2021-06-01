1661f041a6871f8cbe16b4572d2bb69cbff334b9
javi.dombro <javi.dombro@outlook.com>
2021-05-31 22:33:16 -0300

    Celerative Test

---
+++ ConsoleApp1/PostDateFormatter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace HackerRankSolutions
+{
+    public static class PostDateFormatter
+    {
+        public static string Format(DateTime date, DateTime current)
+        {
+            var output = "(s) ago";
+
+            var difference = current - date;
+            if (difference.Minutes == 0)
+            {
+                return "now";
+            }
+            if (difference.Days > 0 && difference.Days <= 7)
+            {
+                return $"{difference.Days} day{output}";
+            }
+            if (difference.Days > 7)
+            {
+                return date.ToString("yyyy-");
+            }
+
+            return difference.Hours > 0 ? $"{difference.Hours} hour{output}" : $"{difference.Minutes} minute{output}";
+        }
+
+        public static int solution(int N)
+        {
+            string number = N.ToString();
+            string result = number;
+            bool positive = true;
+            for (int i = 0; i < number.Length; i++)
+            {
+                if (!int.TryParse(number[i].ToString(), out int aux))
+                {
+                    positive = false;
+                    continue;
+                }
+                if (positive && aux >= 5)
+                {
+                    continue;
+                }
+                if (!positive && aux <= 5)
+                {
+                    continue;
+                }
+                result = number.Insert(i, "5");
+                break;
+            }
+            if (result == number)
+            {
+                result += "5";
+            }
+            return int.Parse(result);
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+﻿using System;
 
 namespace HackerRankSolutions
 {
@@ -7,9 +7,13 @@ namespace HackerRankSolutions
         static void Main(string[] args)
         {
 
+            var solution = PostDateFormatter.solution(-59455);
+            Console.WriteLine(solution);
+            //var diff = PostDateFormatter.Format(DateTime.Now, DateTime.Now);
+            //Console.WriteLine(diff);
 
-            var asds = JaneaSystemsCodingChallenge.countMax(new List<string> { "108 229", "1132 1237", "534 91", "3218 125", "136 222", "237 114", "235 1200" });
-            System.Console.WriteLine(asds);
+            //var asds = JaneaSystemsCodingChallenge.countMax(new List<string> { "108 229", "1132 1237", "534 91", "3218 125", "136 222", "237 114", "235 1200" });
+            //System.Console.WriteLine(asds);
             //var order = JimAndTheOrders.jimOrders(new List<List<int>>
             //{
             //    new List<int>{1,13},new List<int>{2,3}, new List<int>{2,2}, new List<int>{3,3}
